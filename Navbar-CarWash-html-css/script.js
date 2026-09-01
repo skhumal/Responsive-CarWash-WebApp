868734fc5fbbc656a6aa98a19e284dcb7817944a
Candy Khumalo <candykhumalo2023@gmail.com>
2026-09-01 20:10:02 +0200

    clean hero and about section CSS

--- Navbar-CarWash-html-css/script.js
+++ Navbar-CarWash-html-css/script.js
@@ -1,7 +1,7 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+const menu = document.querySelector('#menu-icon');
+const navlist = document.querySelector('.navlist');
 
-menu.onclick = () => {
+menu.addEventListener('click', () => {
   menu.classList.toggle('bx-x');
   navlist.classList.toggle('open');
-}
+});
